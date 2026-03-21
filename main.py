9525d214af663d3fa95ce2842083e5a9fe8785dd
Rahul Singh <2021ugec091@nitjsr.ac.in>
2026-03-21 12:35:26 +0530

    post method added...

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,6 +8,25 @@ app = FastAPI()
 def health_check():
     return { "statusCode" : 200 , "success" : "true"}
 
+@app.get("/blog")
+def getBlogs(limit : int = 10 , published : bool = True):
+    return { "limit": limit , "published" : published , "blogs" : f"Blogs with limit {limit} and published {published}" }
+
+@app.get("/blog/{blog_id}")
+def getBlog(blog_id: int):
+    return { "blog_id" : blog_id }
+
+@app.get("/blog/{blog_id}/comments")
+def getComments(blog_id : int):
+    return { "blog_id" : blog_id , "comments" : ["comment1" , "comment2" , "comment3"] }
 
 
+class  Blog(BaseModel):
+    title: str
+    content: str
+    published: bool
+
+@app.post("/blog")
+def createBlog(blog  : Blog):
+    return { "blog_details": blog , "message" : "Blog created successfully" }
 
